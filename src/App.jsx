0d828e41230f2f63488a5fcfcc5dc6d592d3aabe
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-04-18 14:31:45 +0700

    styling the sidebar and button with tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
   return (
-    <>
+    <main className="h-screen my-8">
       <ProjectsSidebar />
-    </>
+    </main>
   );
 }
 
